2edf08c4da2640eb331255bea90791edab985536
Kelly Innes <kinnes@gmail.com>
2018-12-24 00:20:43 -0500

    Create submittable form

--- project.clj
+++ project.clj
@@ -4,9 +4,10 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
 
-  :dependencies [[org.clojure/clojure "1.9.0"]
+  :dependencies [[org.clojure/clojure "1.10.0"]
                  [org.clojure/clojurescript "1.10.439"]
-                 [reagent "0.8.1"]]
+                 [reagent "0.8.1"]
+                 [cljs-ajax "0.8.0"]]
 
   :plugins [[lein-cljsbuild "1.1.5"]
             [lein-figwheel "0.5.16"]]
